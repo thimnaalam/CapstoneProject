bcfe547a6c6979187e6f0e97772618dffa6a2f9d
thimnaalam <alamthimna@gmail.com>
2024-03-21 13:58:41 +0200

    productsUsers

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,8 @@
 import { users} from '../model/index.js'
 import express from 'express'
 import bodyParser from 'body-parser'
-// import { verifyAToken } from '../middleware/UserAuthentication.js'
-
+import { verifyAToken } from '../middleware/UserAuthentication.js'
 const userRouter = express.Router()
-
 // Fetch users
 userRouter.get('/', (req, res)=>{
     try{
@@ -16,7 +14,7 @@ userRouter.get('/', (req, res)=>{
         })
     }
 })
-//  Fetch user 
+//  Fetch user
 userRouter.get('/:id', (req, res)=>{
     try{
         users.fetchUser(req, res)
@@ -27,14 +25,44 @@ userRouter.get('/:id', (req, res)=>{
         })
     }
 })
-// Add a user 
-userRouter.post('/register',bodyParser.json, (req, res)=>{
+// Add a user
+userRouter.post('/register', bodyParser.json(), (req, res)=>{
     try{
         users.createUser(req, res)
     }catch(e){
         res.json({
             status: res.statusCode,
             msg: 'Failed to add a new user'
+        })
+    }
+})
+userRouter.patch('/update/:id',bodyParser.json(), (req, res)=>{
+    try{
+        users.updateUser(req, res)
+    }catch(e){
+        res.json({
+            status: res.statusCode,
+            msg: 'Failed to update new user'
+        })
+    }
+})
+userRouter.delete('/delete/:id',bodyParser.json(), (req, res)=>{
+    try{
+        users.deleteUser(req, res)
+    }catch(e){
+        res.json({
+            status: res.statusCode,
+            msg: 'Failed to delete new user'
+        })
+    }
+})
+userRouter.post('/login',bodyParser.json(), (req, res)=>{
+    try{
+        users.login(req, res)
+    }catch(e){
+        res.json({
+            status: res.statusCode,
+            msg: 'Failed to login, try again'
         })
     }
 })
