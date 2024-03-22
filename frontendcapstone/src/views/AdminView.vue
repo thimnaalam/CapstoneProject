717a439025aa86ec20c511de76abf206b0283fc4
thimnaalam <alamthimna@gmail.com>
2024-03-22 13:01:40 +0200

    update

--- frontendcapstone/src/views/AdminView.vue
+++ frontendcapstone/src/views/AdminView.vue
@@ -2,6 +2,7 @@
   <div>
     <AddUser @userRegistered="fetchUsers" />
     <UpdateProd :products="products"/>
+    <UpdateProd :events="events"/>
     <table class="table">
       <thead>
         <tr>
@@ -22,7 +23,7 @@
           <td>{{ user.Email }}</td>
           <td>{{ user.UserType }}</td>
           <td>{{ user.Department }}</td>
-            <!-- SVG icon button for editing -->
+            
             <button @click="editUser(user)">
       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -56,14 +57,14 @@
           <td>{{ product.Catorgory }}</td>
           <td>{{ product.Descriptions }}</td>
           <td>{{ product.userID }}</td>
-             <!-- SVG icon button for editing -->
+          
     <button @click="editProduct(product)">
       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
       </svg>
     </button>
-             <!-- SVG icon button for deleting -->
+           
     <button @click="deleteProduct(product.prodID)">
       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
@@ -85,11 +86,16 @@ export default {
     products() {
       return this.$store.state.products;
     },
+    events() {
+      return this.$store.state.events;
+    },
   },
   mounted() {
     this.$store.dispatch("fetchUsers");
     this.$store.dispatch('fetchProducts');
     this.$store.dispatch('UpdateProduct');
+    this.$store.dispatch('fetchEvents');
+    this.$store.dispatch('UpdateEvent');
   },
   methods: {
     async fetchUsers() {
@@ -114,15 +120,9 @@ async deleteUser(userID) {
   this.$router.push("/admin");
 },
     async deleteProduct(prodID) {
-    // const confirmed = confirm("Are you sure you want to delete this product?");
-      // if (confirmed) {
-        // try {
+ 
            this.$store.dispatch("deleteProduct", prodID);
-          // console.log("Product deleted successfully!");
-        // } catch (error) {
-        // console.error("Error deleting product:", error);
-        // }
-      // }
+   
       this.$router.push("/admin");
     },
   },
